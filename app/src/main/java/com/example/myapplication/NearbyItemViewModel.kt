ecd64fc0b75dceaf9fc444ff3316bbb30bc1c658
Jose Martinez <jmarti1997.jm@gmail.com>
2022-12-06 19:43:01 -0600

    clean code

--- app/src/main/java/com/example/myapplication/NearbyItemViewModel.kt
+++ app/src/main/java/com/example/myapplication/NearbyItemViewModel.kt
@@ -13,23 +13,15 @@ public class NearbyItemViewModel(var locationTitle: String, var locationFound: S
         fun createList(): ArrayList<NearbyItemViewModel>{
 
             val items = ArrayList<NearbyItemViewModel>()
-            Log.d("PLEASE FUCKING WORK!!!!!","!!!!!!")
 
             for (test in tests){
-                Log.d("TEST MARKER TITLE",test.get("title").toString())
                 val title = test.get("title").toString()
                 val location : String = test.get("latitude").toString() + ", " + test.get("longitude").toString()
-
-
                 items.add(NearbyItemViewModel(title,location,"IT WORKED!"))
             }
 
-            ///iterative through geocahces
-
-
             return  items
         }
-
 
     }
 
